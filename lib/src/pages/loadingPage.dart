482197c62a61d3f3960bf005a11db70982d36a2c
fardanylabib <fardanylabib@gmail.com>
2021-08-25 12:19:31 +0700

    set firebase for android

--- lib/src/pages/loadingPage.dart
+++ lib/src/pages/loadingPage.dart
@@ -6,14 +6,35 @@ void main() => runApp(LoadingPage());
 class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ListView(
-          children: [
-            // Load a Lottie file from your assets
-            Lottie.asset('assets/loading.json'),
-          ],
-        ),
+    return MaterialApp(title: "Loading App", home: Loading());
+  }
+}
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(vertical: 20),
+            alignment: Alignment.center,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Lottie.asset('assets/loading.json',
+                      width: 200, fit: BoxFit.fill),
+                  Text(
+                    'Memulai aplikasi...',
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ])),
       ),
     );
   }
